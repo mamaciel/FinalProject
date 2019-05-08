58e27a873655994f4d0a18c09078a96114a7aa47
Marcos <mamaciel@ursinus.edu>
2019-05-08 14:34:00 -0400

    uyriyt

--- Node.java
+++ Node.java
@@ -5,8 +5,19 @@ public class Node<K,V>{
     boolean terminal;
     
     HashMap<K, Node> next;
-
+    
     Node getNext(K nextKey){
 	return next.get(nextKey);
+    }
+    
+    V getVal(){
+	return this.value;
+    }
+    
+    void setVal(V value){
+	this.value = value;
+    }
+    void setNext(HashMap<K, Node> next){
+	this.next = next;
     }
 }
